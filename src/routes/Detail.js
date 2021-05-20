75bdec710d319533604bdb1283c8adc5e9f5dd78
hayami226 <59946552+hayami226@users.noreply.github.com>
2021-05-20 21:33:23 +0900

    20210520 Commit

---
+++ src/routes/Detail.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./Detail.css";
+
+class Detail extends React.Component {
+    componentDidMount() {
+        const { location, history } = this.props;
+        if(location.state === undefined) {
+            history.push("/");
+        }
+    }
+
+    render() {
+        const { location } = this.props;
+        if(location.state) {
+            return (
+                <div className="detail">
+                    <h1>{location.state.title}</h1>
+                    <h3>{location.state.year}</h3>
+                    <ul>
+                        {location.state.genres.map((genre, index) => (
+                            <li key={index}>
+                                {genre}
+                            </li>
+                        ))}
+                    </ul>                
+                    <p>
+                        {location.state.summary}
+                    </p>
+                </div>
+            );
+        } else {
+            return null;
+        }
+    }
+}
+
+export default Detail;
